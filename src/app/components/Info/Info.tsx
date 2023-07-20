3fd790cfd1fe49154e0b301c902499ddfeee6609
benbielin <ben_bielin@berkeley.edu>
2023-07-19 23:52:53 -0700

    edit info styling

--- src/app/components/Info/Info.tsx
+++ src/app/components/Info/Info.tsx
@@ -5,34 +5,21 @@ import { Fragment } from "react";
 import { InfoPropsType } from "@/app/types";
 import InfoCard from "./InfoCard";
 
+import styles from "@components/Info/styles.module.css";
+
 const Info = ({ props }: { props: InfoPropsType }) => {
   return (
-    <div
-      style={{
-        textAlign: "center",
-        border: "4px solid #ff8e8e",
-        backgroundColor: "#242424",
-        borderRadius: "12px",
-        margin: "2px 36px 0px 36px",
-      }}
-    >
+    <div className={styles["info-wrapper"]}>
       {props.title}
       {props.infoRows.map((infoRow) => {
         return (
           <Fragment key={infoRow.title}>
-            <div>{infoRow.title}</div>
+            <div className={styles['info-row-title']}>{infoRow.title}</div>
             <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-evenly",
-                textAlign: "justify",
-              }}
+              className={styles['info-row']}
             >
               {infoRow.infoCards.map((infoCard) => {
-                return (
-                  <InfoCard key={infoCard.title} infoCard={infoCard}/>
-                );
+                return <InfoCard key={infoCard.title} infoCard={infoCard} />;
               })}
             </div>
           </Fragment>
