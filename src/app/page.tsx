46b45f1dc5492d530cc664c7186df4f6922fe08f
benbielin <ben_bielin@berkeley.edu>
2023-08-03 21:39:35 -0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 import { useCallback, useState } from "react";
 
-import MainContainer from "@/app/components/MainContainer/MainContainer";
+import MainContainer from "./components/MainContainer/MainContainer";
 import HelloEveryone from "./components/helloEveryone/HelloEveryone";
-import Welcome from "@components/Welcome/Welcome";
+import Welcome from "./components/Welcome/Welcome";
 import Chunk from "@components/Chunk/Chunk";
 import Container from "@components/Container/Container";
 import Intro from "@components/Intro/Intro";
